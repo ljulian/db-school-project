bb9619c209cd6ac508043ccee3d7afa370669e0f
Luis Julian <ljulian2190@gmail.com>
2018-10-28 17:25:56 -0700

    Partially finished implementing Repository class. Need to learn more about LINQ extended methods.

---
+++ Assignment_6/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    interface IRepository<T>:IDisposable
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> GetAll();
+        T GetSingle(
+            Func<T, bool> whereExp, 
+            params Expression<Func<T, object>>[] navigationProperties
+            );
+    }
+}
---
+++ Assignment_6/Repository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    public class Repository<T>: IRepository<T> where T: class
+    {
+        protected DbContext context;
+        public Repository(DbContext datacontext)
+        {
+            this.context = datacontext;
+        }
+
+        public void Insert(T entity)
+        {
+            context.Entry(entity).State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Entry(entity).State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate)
+        {
+            return context.Set<T>().Where(predicate);
+        }
+
+        // Student can be specified to get a collection of
+        // students from the context?
+        public IEnumerable<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetSingle(Func<T, bool> whereExp, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
