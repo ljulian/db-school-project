41b071f4a22ae509a9cf28d7b2fa3f575b7137b3
Luis Julian <ljulian2190@gmail.com>
2018-11-07 10:56:18 -0800

    Printed list of teachers, students, and standards

--- Assignment_6/Program.cs
+++ Assignment_6/Program.cs
@@ -15,7 +15,71 @@ namespace Assignment_6
             //where: function looks for standard. 
             IBusinessLayer businessLayer = new BusinessLayer();
 
+            //addTeacher(businessLayer, "Mike Brown", 2);
 
+
+
+
+            print_all_standards(businessLayer);
+            print_all_teachers(businessLayer);
+            print_all_students(businessLayer);
+
+            Console.ReadLine();
+        }
+
+        static void addTeacher(IBusinessLayer bl, string teacherName, 
+                               int standardID)
+        {
+            Teacher newTeach = new Teacher()
+            {
+                TeacherName = teacherName
+            };
+
+            Standard std = bl.GetStandardByID(standardID);
+            if (std != null)
+            {
+                // TODO Just set the standardID
+                // TODO ask about this case.
+                newTeach.Standard = std;
+            }
+            else
+            {
+                newTeach.Standard = new Standard()
+                {
+                    StandardName = "New Standard 1"
+                };
+            }
+            bl.AddTeacher(newTeach);
+        }
+
+        static void print_all_standards(IBusinessLayer bl)
+        {
+            IList<Standard> st = bl.GetAllStandards();
+            foreach(var elm in st)
+            {
+                string message = "Name: {0}";
+                Console.WriteLine(string.Format(message, elm.StandardName));
+            }
+        }
+
+        static void print_all_students(IBusinessLayer bl)
+        {
+            IList<Student> st = bl.GetAllStudents();
+            foreach (var elm in st)
+            {
+                string message = "Name: {0}";
+                Console.WriteLine(string.Format(message, elm.StudentName));
+            }
+        }
+
+        static void print_all_teachers(IBusinessLayer bl)
+        {
+            IList<Teacher> st = bl.GetAllTeachers();
+            foreach (var elm in st)
+            {
+                string message = "Name: {0}";
+                Console.WriteLine(string.Format(message, elm.TeacherName));
+            }
         }
     }
 }
